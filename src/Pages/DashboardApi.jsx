f5b3270d824ef0a91b9d9a33ca6ad94d9585e806
Abdullah Khan <abdullah.khan53145@gmail.com>
2023-04-01 19:20:49 -0700

    Feat : Completed the Extention page and fully responsive

--- src/Pages/DashboardApi.jsx
+++ src/Pages/DashboardApi.jsx
@@ -1,10 +1,18 @@
 import React from "react";
-
+import { DocumentTextIcon } from "@heroicons/react/24/outline";
 function DashboardApi() {
   return (
-    <div>
-      <h1>This is API page</h1>
-    </div>
+    <section className="md:mx-16 mx-5 mt-12 flex items-start flex-col gap-8">
+      {/* Heading */}
+      <h2 className="text-2xl font-semibold">
+        Please contact us to get an access support@getmoretracks.com
+      </h2>
+      {/* Documentaion of API buttons */}
+      <button className="btn-primary">
+        <DocumentTextIcon className="w-4 h-4" />
+        <span>API Documentation</span>
+      </button>
+    </section>
   );
 }
 
--- src/Pages/DashboardExtention.jsx
+++ src/Pages/DashboardExtention.jsx
@@ -1,10 +1,24 @@
 import React from "react";
-
+import { ArrowDownTrayIcon } from "@heroicons/react/24/outline";
 function DashboardExtention() {
   return (
-    <div>
-      <h1>This is Extention page</h1>
-    </div>
+    <section className="md:mx-16 mx-5 mt-12 flex items-start flex-col gap-8">
+      {/* Heading */}
+      <h2 className="text-2xl font-semibold">Getmoretracks Chrome extension</h2>
+      {/* Extention download button */}
+      <button className="btn-primary">
+        <ArrowDownTrayIcon className="w-4 h-4" />
+        <span>Download Extention</span>
+      </button>
+      <iframe
+        className="w-full md:h-[90vh] sm:h-[40vh] h-[30rem] mt-8"
+        src="https://www.youtube.com/embed/SRd4i0oPjQ8"
+        title="getmoretracks.com Chrome app"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      ></iframe>
+    </section>
   );
 }
 
