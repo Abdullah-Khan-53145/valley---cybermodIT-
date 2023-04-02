3e477f0d3ab28d07c48b377832ea729b1ce91ab0
Abdullah Khan <abdullah.khan53145@gmail.com>
2023-04-01 19:41:14 -0700

    Feat : Completed the Settings page and made it fully responsive and functional

--- src/Pages/DashboardSettings.jsx
+++ src/Pages/DashboardSettings.jsx
@@ -1,10 +1,69 @@
-import React from "react";
-
+import React, { useState } from "react";
+import {
+  EnvelopeIcon,
+  EyeIcon,
+  EyeSlashIcon,
+  BookmarkSquareIcon,
+} from "@heroicons/react/24/outline";
 function DashboardSettings() {
+  const [password, setPassword] = useState({ current: true, new: true });
+  const handleClick = () => {};
+
   return (
-    <div>
-      <h1>This is Settings page</h1>
-    </div>
+    <section className="md:mx-16 mx-5 mt-16 flex flex-col gap-8">
+      {/* Form to manipulate the setting of profile */}
+      <form className="max-w-[25rem] flex flex-col gap-8 items-start p-8 bg-white">
+        {/* Email */}
+        <div className="flex p-4 border-b items-center justify-between w-full">
+          <input type="text" placeholder="Email" />
+          <EnvelopeIcon className="w-5 h-5 text-gray-950" />
+        </div>
+        {/* Current Password */}
+        <div className="flex p-4 border-b items-center justify-between w-full">
+          <input
+            type={password.current ? "text" : "password"}
+            placeholder="Current Password"
+          />
+          {password.current ? (
+            <EyeIcon
+              onClick={() =>
+                setPassword({ ...password, current: !password.current })
+              }
+              className="w-5 h-5 text-gray-950 cursor-pointer hover:text-gray-700 transition-colors"
+            />
+          ) : (
+            <EyeSlashIcon
+              onClick={() =>
+                setPassword({ ...password, current: !password.current })
+              }
+              className="w-5 h-5 text-gray-950 cursor-pointer hover:text-gray-700 transition-colors"
+            />
+          )}
+        </div>
+        {/* New Password */}
+        <div className="flex p-4 border-b items-center justify-between w-full">
+          <input
+            type={password.new ? "text" : "password"}
+            placeholder="New Password"
+          />
+          {password.new ? (
+            <EyeIcon
+              onClick={() => setPassword({ ...password, new: !password.new })}
+              className="w-5 h-5 text-gray-950 cursor-pointer hover:text-gray-700 transition-colors"
+            />
+          ) : (
+            <EyeSlashIcon
+              onClick={() => setPassword({ ...password, new: !password.new })}
+              className="w-5 h-5 text-gray-950 cursor-pointer hover:text-gray-700 transition-colors"
+            />
+          )}
+        </div>
+        <button className="btn-primary px-10">
+          <BookmarkSquareIcon className="w-5 h-5 text-white" />
+          <span>Save Setting</span>
+        </button>
+      </form>
+    </section>
   );
 }
 
